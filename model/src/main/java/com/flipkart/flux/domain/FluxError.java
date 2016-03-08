75494d9761e4958f977eab67642e279b45b128ac
Regunath B <regunathb@gmail.com>
2016-03-08 14:33:40 +0530

    Now extends from RuntimeException

--- model/src/main/java/com/flipkart/flux/domain/FluxError.java
+++ model/src/main/java/com/flipkart/flux/domain/FluxError.java
@@ -13,11 +13,52 @@
 
 package com.flipkart.flux.domain;
 
-public class FluxError {
-    private String errorJson;
+/**
+ * <code>FluxError</code> defines an error in the Flux runtime.
+ * 
+ * @author Yogesh
+ * @author regunath.balasubramanian
+ * 
+ */
+public class FluxError extends RuntimeException {
+	
+	/** The type of error*/
     private ErrorType type;
-
+    /** The error message*/
+    private String errorMessage;
+    /** The root cause Throwable, if any*/
+    private Throwable rootCause;
+    /** Enum of errro types*/
     public enum ErrorType {
         runtime,timeout
     }
+    
+    /** Constructor */
+	public FluxError(ErrorType type, String errorMessage, Throwable rootCause) {
+		super();
+		this.type = type;
+		this.errorMessage = errorMessage;
+		this.rootCause = rootCause;
+	}
+
+	/** Accessor/Mutator methods*/
+	public ErrorType getType() {
+		return type;
+	}
+	public void setType(ErrorType type) {
+		this.type = type;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public Throwable getRootCause() {
+		return rootCause;
+	}
+	public void setRootCause(Throwable rootCause) {
+		this.rootCause = rootCause;
+	}
+    
 }
