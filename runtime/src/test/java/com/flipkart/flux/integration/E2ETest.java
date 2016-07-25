62c4a2156db4c3fda6492a5df985fed291853d25
Shyam Kumar Akirala <shyam.akirala@flipkart.com>
2016-07-25 16:35:13 +0530

    changed E2E test case

--- runtime/src/test/java/com/flipkart/flux/integration/E2ETest.java
+++ runtime/src/test/java/com/flipkart/flux/integration/E2ETest.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -62,11 +61,8 @@ public class E2ETest {
         final Long smId = smInDb.stream().findFirst().get().getId();
         assertThat(smInDb).hasSize(1);
         assertThat(eventsDAO.findBySMInstanceId(smId)).hasSize(3);
-        /* The following will return true only if the latch has been counted down as part of execution in another thread */
-        assertThat(simpleWorkflow.getCountDownLatchForSimpleIntegerReturnTask().await(1, TimeUnit.SECONDS)).isTrue();
-        assertThat(simpleWorkflow.getCountDownLatchForSimpleStringReturnTask().await(1, TimeUnit.SECONDS)).isTrue();
-        assertThat(simpleWorkflow.getCountDownLatchForSimpleIntegerAndStringTask().await(1, TimeUnit.SECONDS)).isTrue();
 
-        /* See if Akka Test suite can be used to verify akka interactions, not needed though */
+        /** All the events should be in triggered state after execution*/
+        assertThat(eventsDAO.findTriggeredEventsNamesBySMId(smId)).hasSize(3);
     }
 }
