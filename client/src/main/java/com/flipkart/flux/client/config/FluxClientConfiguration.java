7a7dff3b3363191f7729d824532b08da1ef6dd96
Ali <ali.nalawala@flipkart.com>
2016-10-10 17:07:06 +0530

    Using a Guice provided configuration for building the flux runtime connector
    This configuration, internally will come from one of client's guice modules. Example of a client provider
    
    ```java
    @Produces
    @Singleton
    FluxConfiguration providesFluxConfiguration(Provider<ManagerConfiguration> configuration) {
        return configuration.get().getFluxConfiguration();
    }
    ```

---
+++ client/src/main/java/com/flipkart/flux/client/config/FluxClientConfiguration.java
@@ -0,0 +1,32 @@
+package com.flipkart.flux.client.config;
+
+public class FluxClientConfiguration {
+
+  private String url;
+  private long socketTimeout = 1000;
+  private long connectionTimeout = 1000;
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public long getSocketTimeout() {
+    return socketTimeout;
+  }
+
+  public void setSocketTimeout(long socketTimeout) {
+    this.socketTimeout = socketTimeout;
+  }
+
+  public long getConnectionTimeout() {
+    return connectionTimeout;
+  }
+
+  public void setConnectionTimeout(long connectionTimeout) {
+    this.connectionTimeout = connectionTimeout;
+  }
+}
