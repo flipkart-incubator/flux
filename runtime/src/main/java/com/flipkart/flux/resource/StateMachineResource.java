7666f5d2b724e2eef372f7b0c38308ae654b06dc
Gaurav Ashok <gaurav.ashok@flipkart.com>
2016-11-14 23:59:35 +0530

    Removing unused import.

--- runtime/src/main/java/com/flipkart/flux/resource/StateMachineResource.java
+++ runtime/src/main/java/com/flipkart/flux/resource/StateMachineResource.java
@@ -34,7 +34,6 @@ import com.flipkart.flux.representation.StateMachinePersistenceService;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
