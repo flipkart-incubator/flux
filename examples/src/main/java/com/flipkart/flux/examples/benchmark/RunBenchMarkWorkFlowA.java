ed7ca654fa7b699be1a944a358b9337cf908a17a
amitkumar94 <amitkumar.o@flipkart.com>
2017-11-16 18:05:17 +0530

    added workflow to simulate latent tasks and stuff

--- examples/src/main/java/com/flipkart/flux/examples/benchmark/RunBenchMarkWorkFlowA.java
+++ examples/src/main/java/com/flipkart/flux/examples/benchmark/RunBenchMarkWorkFlowA.java
@@ -7,16 +7,20 @@ import com.google.inject.Injector;
 
 public class RunBenchMarkWorkFlowA {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
          /* Initialise _your_ module*/
         final Injector injector = Guice.createInjector(new FluxClientComponentModule(), new FluxClientInterceptorModule());
 
         /* Note that we are using guice aop for now, hence your workflow instances need to use guice */
         BenchmarkWorkFlowA benchmarkWorkFlowA = injector.getInstance(BenchmarkWorkFlowA.class);
+        BenchmarkWorkflowLatentSimulation benchmarkWorkFlowLatent = injector.getInstance(BenchmarkWorkflowLatentSimulation.class);
+
 
         /* Lets invoke our workflow */
         System.out.println("[Main] Starting BenchMarkWorkFlowA  execution");
-        for(int i = 1  ;i <= 1000; i++)
-        benchmarkWorkFlowA.initA(new EventTypeInteger((int)Math.random()*i));
+        for (int i = 1; i <= 10000 ; i++) {
+            benchmarkWorkFlowA.initA(new EventTypeInteger((int) Math.random() * i));
+            benchmarkWorkFlowLatent.initLatent(new EventTypeInteger(i));
+        }
     }
 }
