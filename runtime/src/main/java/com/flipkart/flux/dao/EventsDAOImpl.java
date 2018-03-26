80704d2873eabdc434724da9783d8bbe560b3ff6
amitkumar94 <amitkumar.o@flipkart.com>
2018-03-27 01:38:57 +0530

    updated query to check if the SQl grammar exception comes again

--- runtime/src/main/java/com/flipkart/flux/dao/EventsDAOImpl.java
+++ runtime/src/main/java/com/flipkart/flux/dao/EventsDAOImpl.java
@@ -118,7 +118,10 @@ public class EventsDAOImpl extends AbstractDAO<Event> implements EventsDAO {
     @Transactional
     @SelectDataSource(type = DataSourceType.READ_WRITE, storage = Storage.SHARDED)
     public Map<String, Event.EventStatus> getAllEventsNameAndStatus(String stateMachineInstanceId, boolean forUpdate) {
-        SQLQuery sqlQuery = currentSession().createSQLQuery("Select name, status from Events where stateMachineInstanceId = " + stateMachineInstanceId + (forUpdate ? " for update" : ""));
+        SQLQuery sqlQuery = currentSession().createSQLQuery("Select name, status from Events where stateMachineInstanceId ='" + stateMachineInstanceId + (forUpdate ? "' for update" : "''"));
+//        Query query = currentSession().createQuery("select name, status from Event where stateMachineInstanceId = :stateMachineInstanceId for update");
+//        query.setString("stateMachineInstanceId", stateMachineInstanceId);
+
         List<Object[]> eventRows = sqlQuery.list();
         Map<String, Event.EventStatus> eventStatusMap = new HashMap<>();
 
