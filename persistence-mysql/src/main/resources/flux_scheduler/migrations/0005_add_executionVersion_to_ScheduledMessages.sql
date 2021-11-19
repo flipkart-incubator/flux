06fcb021ad7b4f64b7bfbc2c120ef8a475236d52
Shyam <shyam.akirala@flipkart.com>
2021-11-19 21:45:52 +0530

    Removed duplicate migration file for ScheduledMessage executionVersion.

--- persistence-mysql/src/main/resources/flux_scheduler/migrations/0005_add_executionVersion_to_ScheduledMessages.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset akif.khan:5 runOnChange:false
-
-ALTER TABLE `ScheduledMessages`
-  DROP PRIMARY KEY,
-  ADD COLUMN `executionVersion` SMALLINT UNSIGNED NOT NULL,
-  ADD PRIMARY KEY (`taskId`,`stateMachineId`,`executionVersion`);
