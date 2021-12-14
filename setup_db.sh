ff615a10fb0fa497ba479c4bef92414d1ccaabf8
regu.b <regunathb@gmail.com>
2021-12-14 18:10:43 +0530

    Fixes DB migrations, Jetty dependency and FSM view display issues

--- setup_db.sh
+++ setup_db.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 echo "drop database if exists flux" | mysql -uroot
 echo "create database flux" | mysql -uroot
-mvn -pl :persistence-mysql liquibase:update
+mvn -pl :persistence-mysql liquibase:update -Dliquibase-maven-plugin.dbname=flux
 echo "drop database if exists flux_scheduler" | mysql -uroot
 echo "create database flux_scheduler" | mysql -uroot
 mvn -pl :persistence-mysql liquibase:update -Dliquibase-maven-plugin.dbname=flux_scheduler
