7f411664d32283c00e57bf65aae59a82e1b9a655
regu.b <regunathb@gmail.com>
2021-12-17 16:40:04 +0530

    Fixes minor EventProxy connector issues

--- client/src/main/java/com/flipkart/flux/client/runtime/EventProxyConnector.java
+++ client/src/main/java/com/flipkart/flux/client/runtime/EventProxyConnector.java
@@ -32,9 +32,10 @@ public class EventProxyConnector extends FluxRuntimeConnectorHttpImpl {
     private static final String EXTERNAL = "external";
 
     @Inject
-    public EventProxyConnector(@Named("eventProxyForMigration.endpoint") String endpoint, FluxClientConfiguration fluxClientConfiguration) {
+    public EventProxyConnector(@Named("eventProxyForMigration.endpoint") String endpoint, FluxClientConfiguration fluxClientConfiguration, 
+        @Named("eventProxyForMigration.targetClientId") String targetClientId) {
         super(fluxClientConfiguration.getConnectionTimeout(), fluxClientConfiguration.getSocketTimeout(),
-                endpoint, new ObjectMapper(), SharedMetricRegistries.getOrCreate("mainMetricRegistry"), endpoint);
+                endpoint, new ObjectMapper(), SharedMetricRegistries.getOrCreate("mainMetricRegistry"), targetClientId);
     }
 
     @Override
