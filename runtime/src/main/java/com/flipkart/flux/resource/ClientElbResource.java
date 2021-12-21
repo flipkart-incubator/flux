9e22d9735ac342c5db7ee81cdf61a14a387de6b0
regu.b <regunathb@gmail.com>
2021-12-21 11:00:01 +0530

    Disables Update and Delete APIs because an update on a stateless node does not propagate to other stateless nodes

--- runtime/src/main/java/com/flipkart/flux/resource/ClientElbResource.java
+++ runtime/src/main/java/com/flipkart/flux/resource/ClientElbResource.java
@@ -1,7 +1,5 @@
 package com.flipkart.flux.resource;
 
-import static com.flipkart.flux.constant.RuntimeConstants.DEFAULT_ELB_ID;
-
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -14,7 +12,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,6 +26,7 @@ import com.google.inject.Singleton;
 
 /**
  * API for CRUD operations on Client Cluster ELB details
+ *
  * @author akif.khan
  */
 @Singleton
@@ -36,180 +34,180 @@ import com.google.inject.Singleton;
 @Named
 public class ClientElbResource {
 
-    private static final Logger logger = LogManager.getLogger(ClientElbResource.class);
+  private static final Logger logger = LogManager.getLogger(ClientElbResource.class);
 
-    private ClientElbPersistenceService clientElbPersistenceService;
+  private ClientElbPersistenceService clientElbPersistenceService;
 
-    @Inject
-    public ClientElbResource(ClientElbPersistenceService clientElbPersistenceService
-    ) {
-        this.clientElbPersistenceService = clientElbPersistenceService;
+  @Inject
+  public ClientElbResource(ClientElbPersistenceService clientElbPersistenceService
+  ) {
+    this.clientElbPersistenceService = clientElbPersistenceService;
 
+  }
+
+  /**
+   * Create and persist new client's ELB details viz. ElbURL and ClientId
+   *
+   * @param clientId     Unique Id specific to a Client Cluster
+   * @param clientElbUrl Client ELB URL Address
+   */
+  @POST
+  @Path("/create")
+  @Timed
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createClientElb(@QueryParam("clientId") String clientId,
+      @QueryParam("clientElbUrl") String clientElbUrl) {
+    if (clientElbUrl == null || clientId == null) {
+      return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
+          "ClientElbUrl or ClientId cannot be null").build();
+    } else {
+      try {
+        URL verifyingURL = new URL(clientElbUrl);
+        verifyingURL.toURI();
+        if (verifyingURL.getHost().length() < 1 || verifyingURL.getPath().length() > 0) {
+          throw new MalformedURLException();
+        }
+        ClientElbDefinition clientElbDefinition = new ClientElbDefinition(clientId, clientElbUrl);
+        ClientElb clientElb = clientElbPersistenceService.persistClientElb(
+            clientElbDefinition.getId(), clientElbDefinition);
+        return Response.status(Response.Status.CREATED.getStatusCode()).entity(clientElb.getId())
+            .build();
+      } catch (MalformedURLException ex) {
+        logger.error(
+            "Malformed URL exception(no path allowed because Elb Url doesn't contain path) {} {} ",
+            ex.getMessage(), ex.getStackTrace());
+        return Response.status(Response.Status.BAD_REQUEST).entity("MalformedURLException").build();
+      } catch (URISyntaxException ex) {
+        logger.error("URI Syntax Exception {} {} ", ex.getMessage(), ex.getStackTrace());
+        return Response.status(Response.Status.BAD_REQUEST).entity("URISyntaxException").build();
+      } catch (ConstraintViolationException ex) {
+        //In case of Duplicate clientId, return http code 409 conflict
+        if (ex.getCause() != null) {
+          if (ex.getCause().getMessage().toLowerCase().contains("duplicate entry")) {
+            return Response.status(Response.Status.CONFLICT.getStatusCode()).entity(
+                ex.getCause().getMessage()).build();
+
+          }
+        }
+        logger.error("Constraint Violation during creating ClientElb entry" +
+                " with id {} {} {}", clientId, ex.getCause().getMessage(),
+            ex.getStackTrace());
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(
+            ex.getCause() != null ? ex.getCause().getMessage() : null).build();
+
+      } catch (Exception ex) {
+        logger.error("Exception occured in ClientElb create {} {} ", ex.getMessage(),
+            ex.getStackTrace());
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+      }
+    }
+  }
+
+  /**
+   * Query and returns ClientElbUrl identified by input @param clientId
+   *
+   * @param clientId Unique Id specific to a Client Cluster
+   */
+  @GET
+  @Path("/findById")
+  @Timed
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response findByIdClientElb(@QueryParam("clientId") String clientId) {
+
+    if (clientId == null) {
+      return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
+          "ClientId cannot be null").build();
     }
 
-    /**
-     * Create and persist new client's ELB details viz. ElbURL and ClientId
-     *
-     * @param clientId Unique Id specific to a Client Cluster
-     * @param clientElbUrl Client ELB URL Address
-     */
-    @POST
-    @Path("/create")
-    @Timed
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response createClientElb(@QueryParam("clientId") String clientId,
-                                @QueryParam("clientElbUrl") String clientElbUrl) {
-        if(clientElbUrl == null || clientId == null)
-            return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
-                    "ClientElbUrl or ClientId cannot be null").build();
-        else {
-            try {
-                URL verifyingURL = new URL(clientElbUrl);
-                verifyingURL.toURI();
-                if(verifyingURL.getHost().length() < 1 || verifyingURL.getPath().length() > 0) {
-                    throw new MalformedURLException();
-                }
-                ClientElbDefinition clientElbDefinition = new ClientElbDefinition(clientId, clientElbUrl);
-                ClientElb clientElb = clientElbPersistenceService.persistClientElb(
-                        clientElbDefinition.getId(), clientElbDefinition);
-                return Response.status(Response.Status.CREATED.getStatusCode()).entity(clientElb.getId()).build();
-            }
-            catch(MalformedURLException ex) {
-                logger.error("Malformed URL exception(no path allowed because Elb Url doesn't contain path) {} {} ",
-                        ex.getMessage(), ex.getStackTrace());
-                return Response.status(Response.Status.BAD_REQUEST).entity("MalformedURLException").build();
-            }
-            catch(URISyntaxException ex) {
-                logger.error("URI Syntax Exception {} {} ", ex.getMessage(), ex.getStackTrace());
-                return Response.status(Response.Status.BAD_REQUEST).entity("URISyntaxException").build();
-            }
-            catch (ConstraintViolationException ex) {
-                //In case of Duplicate clientId, return http code 409 conflict
-                if (ex.getCause() != null) {
-                    if (ex.getCause().getMessage().toLowerCase().contains("duplicate entry")) {
-                        return Response.status(Response.Status.CONFLICT.getStatusCode()).entity(
-                            ex.getCause().getMessage()).build();
-
-                    }
-                }
-                logger.error("Constraint Violation during creating ClientElb entry" +
-                        " with id {} {} {}", clientId, ex.getCause().getMessage(),
-                    ex.getStackTrace());
-                return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(
-                    ex.getCause() != null ? ex.getCause().getMessage() : null).build();
-
-            }
-            catch(Exception ex) {
-                logger.error("Exception occured in ClientElb create {} {} ", ex.getMessage(), ex.getStackTrace());
-                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-            }
-        }
+    try {
+      String clientElbUrl = clientElbPersistenceService.findByIdClientElb(clientId);
+      if (clientElbUrl == null) {
+        return Response.status(Response.Status.NOT_FOUND.getStatusCode())
+            .entity("ClientElb with Id: " + clientId + " not found").build();
+      }
+      return Response.status(Response.Status.FOUND.getStatusCode()).entity(clientElbUrl).build();
+    } catch (Exception ex) {
+      logger.error("findById failed for input: " + clientId + " {} {} ", ex.getMessage(),
+          ex.getStackTrace());
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
+  }
 
-    /**
-     * Query and returns ClientElbUrl identified by input @param clientId
-     *
-     * @param clientId Unique Id specific to a Client Cluster
-     *
-     */
-    @GET
-    @Path("/findById")
-    @Timed
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response findByIdClientElb(@QueryParam("clientId") String clientId) {
-
-        if(clientId == null) {
-            return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
-                    "ClientId cannot be null").build();
-        }
-
-        try {
-            String clientElbUrl = clientElbPersistenceService.findByIdClientElb(clientId);
-            if (clientElbUrl == null) {
-                return Response.status(Response.Status.NOT_FOUND.getStatusCode())
-                        .entity("ClientElb with Id: " + clientId + " not found").build();
-            }
-            return Response.status(Response.Status.FOUND.getStatusCode()).entity(clientElbUrl).build();
-        }
-        catch(Exception ex) {
-            logger.error("findById failed for input: "+ clientId + " {} {} ", ex.getMessage(), ex.getStackTrace());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    /**
-     * Update and persist existing client's ELB details viz. ElbURL and ClientId
-     *
-     * @param clientId Unique Id specific to a Client Cluster
-     * @param clientElbUrl Elb Url that's supposed to be updated.
-     */
-    @POST
-    @Path("/update")
-    @Timed
-    public Response updateClientElb(@QueryParam("clientId") String clientId,
-                                    @QueryParam("clientElbUrl") String clientElbUrl) {
-
-        if(clientId == null || clientElbUrl == null) {
-            return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
-                    "ClientId OR ClientElbUrl cannot be null").build();
-        }
-        try {
-            URL verifyingURL = new URL(clientElbUrl);
-            verifyingURL.toURI();
-            if(verifyingURL.getHost().length() < 1 || verifyingURL.getPath().length() > 0) {
-                throw new MalformedURLException();
-            }
-            if(clientId == DEFAULT_ELB_ID) {
-                return Response.status(Status.FORBIDDEN).entity("Cannot update reserved clientId: " +
-                    clientId).build();
-            }
-            clientElbPersistenceService.updateClientElb(clientId, clientElbUrl);
-        }
-        catch(MalformedURLException ex) {
-            logger.error("Malformed URL exception(no path allowed because Elb Url doesn't contain path) {} {} ",
-                    ex.getMessage(), ex.getStackTrace());
-            return Response.status(Response.Status.BAD_REQUEST).entity("MalformedURLException").build();
-        }
-        catch(URISyntaxException ex) {
-            logger.error("URI Syntax Exception {} {} ", ex.getMessage(), ex.getStackTrace());
-            return Response.status(Response.Status.BAD_REQUEST).entity("URISyntaxException").build();
-        }
-        catch(Exception ex) {
-            logger.error("Update failed for input: "+ clientId + " " + clientElbUrl + " {} {} "
-                    , ex.getMessage(), ex.getStackTrace());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-        return Response.status(Response.Status.ACCEPTED).build();
-    }
-
-    /**
-     * Delete client's ELB details viz. ElbURL and ClientId
-     *
-     * @param clientId Unique Id specific to a Client Cluster
-     *
-     */
-    @POST
-    @Path("/delete")
-    @Timed
-    public Response deleteClientElb(@QueryParam("clientId") String clientId) {
-
-        if(clientId == null) {
-            return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
-                    "ClientId cannot be null").build();
-        }
-
-        if(clientId == DEFAULT_ELB_ID) {
-            return Response.status(Status.FORBIDDEN).entity("Cannot delete reserved clientId: " +
-                clientId).build();
-        }
-
-        try {
-            clientElbPersistenceService.deleteClientElb(clientId);
-        }
-        catch(Exception ex) {
-            logger.error("Deletion failed for input: "+ clientId + " {} {} ", ex.getMessage(), ex.getStackTrace());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-        return Response.status(Response.Status.ACCEPTED).build();
-    }
+/*
+Disabling Update and delete because an update on a stateless box doesn't propagate to other stateless boxes
+where query didn't arrive. This was relevant when there was no cache for data, with cache in-between
+Update should be done as  "Delete from DB, restart daemon svc and insert via ClientElb API"
+ */
+//  /**
+//   * Update and persist existing client's ELB details viz. ElbURL and ClientId
+//   *
+//   * @param clientId     Unique Id specific to a Client Cluster
+//   * @param clientElbUrl Elb Url that's supposed to be updated.
+//   */
+//  @POST
+//  @Path("/update")
+//  @Timed
+//  public Response updateClientElb(@QueryParam("clientId") String clientId,
+//      @QueryParam("clientElbUrl") String clientElbUrl) {
+//
+//    if (clientId == null || clientElbUrl == null) {
+//      return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
+//          "ClientId OR ClientElbUrl cannot be null").build();
+//    }
+//    try {
+//      URL verifyingURL = new URL(clientElbUrl);
+//      verifyingURL.toURI();
+//      if (verifyingURL.getHost().length() < 1 || verifyingURL.getPath().length() > 0) {
+//        throw new MalformedURLException();
+//      }
+//      if (clientId == DEFAULT_ELB_ID) {
+//        return Response.status(Status.FORBIDDEN).entity("Cannot update reserved clientId: " +
+//            clientId).build();
+//      }
+//      clientElbPersistenceService.updateClientElb(clientId, clientElbUrl);
+//    } catch (MalformedURLException ex) {
+//      logger.error(
+//          "Malformed URL exception(no path allowed because Elb Url doesn't contain path) {} {} ",
+//          ex.getMessage(), ex.getStackTrace());
+//      return Response.status(Response.Status.BAD_REQUEST).entity("MalformedURLException").build();
+//    } catch (URISyntaxException ex) {
+//      logger.error("URI Syntax Exception {} {} ", ex.getMessage(), ex.getStackTrace());
+//      return Response.status(Response.Status.BAD_REQUEST).entity("URISyntaxException").build();
+//    } catch (Exception ex) {
+//      logger.error("Update failed for input: " + clientId + " " + clientElbUrl + " {} {} "
+//          , ex.getMessage(), ex.getStackTrace());
+//      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//    }
+//    return Response.status(Response.Status.ACCEPTED).build();
+//  }
+//
+//  /**
+//   * Delete client's ELB details viz. ElbURL and ClientId
+//   *
+//   * @param clientId Unique Id specific to a Client Cluster
+//   */
+//  @POST
+//  @Path("/delete")
+//  @Timed
+//  public Response deleteClientElb(@QueryParam("clientId") String clientId) {
+//
+//    if (clientId == null) {
+//      return Response.status(Response.Status.BAD_REQUEST.getStatusCode()).entity(
+//          "ClientId cannot be null").build();
+//    }
+//
+//    if (clientId == DEFAULT_ELB_ID) {
+//      return Response.status(Status.FORBIDDEN).entity("Cannot delete reserved clientId: " +
+//          clientId).build();
+//    }
+//
+//    try {
+//      clientElbPersistenceService.deleteClientElb(clientId);
+//    } catch (Exception ex) {
+//      logger.error("Deletion failed for input: " + clientId + " {} {} ", ex.getMessage(),
+//          ex.getStackTrace());
+//      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//    }
+//    return Response.status(Response.Status.ACCEPTED).build();
+//  }
 }
--- runtime/src/test/java/com/flipkart/flux/resource/ClientElbResourceTest.java
+++ runtime/src/test/java/com/flipkart/flux/resource/ClientElbResourceTest.java
@@ -1,21 +1,21 @@
 package com.flipkart.flux.resource;
 
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import com.flipkart.flux.api.ClientElbDefinition;
-import com.flipkart.flux.domain.ClientElb;
-import com.flipkart.flux.representation.ClientElbPersistenceService;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import com.flipkart.flux.api.ClientElbDefinition;
+import com.flipkart.flux.domain.ClientElb;
+import com.flipkart.flux.representation.ClientElbPersistenceService;
 
 /**
  * @author akif.khan
@@ -23,111 +23,124 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class ClientElbResourceTest {
 
-    ClientElbPersistenceService clientElbPersistenceService = mock(ClientElbPersistenceService.class);
+  ClientElbPersistenceService clientElbPersistenceService = mock(ClientElbPersistenceService.class);
 
-    ClientElbResource clientElbResource = new ClientElbResource(clientElbPersistenceService);
+  ClientElbResource clientElbResource = new ClientElbResource(clientElbPersistenceService);
 
-    @Test
-    public void testCreateClientElb() {
+  @Test
+  public void testCreateClientElb() {
 
-        ClientElbDefinition clientElbDefinition = new ClientElbDefinition(
-                "id1", "http://10.3.2.3");
-        ClientElb clientElb = new ClientElb("id1", "http://10.3.2.3");
-        when(clientElbPersistenceService.persistClientElb("id1", clientElbDefinition))
-                .thenReturn(clientElb);
+    ClientElbDefinition clientElbDefinition = new ClientElbDefinition(
+        "id1", "http://10.3.2.3");
+    ClientElb clientElb = new ClientElb("id1", "http://10.3.2.3");
+    when(clientElbPersistenceService.persistClientElb("id1", clientElbDefinition))
+        .thenReturn(clientElb);
 
-        Response response_1 = clientElbResource.createClientElb(
-                "id1", "http://10.3.2.3");
-        Assertions.assertThat(response_1.getStatus()).isEqualTo(Response.Status.CREATED.getStatusCode());
+    Response response_1 = clientElbResource.createClientElb(
+        "id1", "http://10.3.2.3");
+    Assertions.assertThat(response_1.getStatus())
+        .isEqualTo(Response.Status.CREATED.getStatusCode());
 
-        Response response_2 = clientElbResource.createClientElb("id1", null);
-        Assertions.assertThat(response_2.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+    Response response_2 = clientElbResource.createClientElb("id1", null);
+    Assertions.assertThat(response_2.getStatus())
+        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
 
-        Response response_3 = clientElbResource.createClientElb("id1", "http:///10.2");
-        Assertions.assertThat(response_3.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+    Response response_3 = clientElbResource.createClientElb("id1", "http:///10.2");
+    Assertions.assertThat(response_3.getStatus())
+        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
 
-        Response response_4 = clientElbResource.createClientElb(null, "http://10.5.4.3");
-        Assertions.assertThat(response_4.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+    Response response_4 = clientElbResource.createClientElb(null, "http://10.5.4.3");
+    Assertions.assertThat(response_4.getStatus())
+        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
 
-        Response response_5 = clientElbResource.createClientElb("id1", "http://10.5.4.3/api");
-        Assertions.assertThat(response_5.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+    Response response_5 = clientElbResource.createClientElb("id1", "http://10.5.4.3/api");
+    Assertions.assertThat(response_5.getStatus())
+        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
 
-        verify(clientElbPersistenceService, times(1)).persistClientElb(
-                "id1", clientElbDefinition);
-        verifyNoMoreInteractions(clientElbPersistenceService);
-    }
-
-
-    @Test
-    public void testFindById() {
-
-        when(clientElbPersistenceService.findByIdClientElb("id2"))
-                .thenReturn("http://10.3.2.3");
-
-        Response response_1 = clientElbResource.findByIdClientElb(null);
-        Assertions.assertThat(response_1.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
-
-        Response response_2 = clientElbResource.findByIdClientElb("id2");
-        Assertions.assertThat(response_2.getStatus()).isEqualTo(Response.Status.FOUND.getStatusCode());
-
-        when(clientElbPersistenceService.findByIdClientElb("id2"))
-                .thenReturn(null);
-
-        Response response_3 = clientElbResource.findByIdClientElb("id2");
-        Assertions.assertThat(response_3.getStatus()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
-
-        verify(clientElbPersistenceService, times(2)).findByIdClientElb(
-                "id2");
-        verifyNoMoreInteractions(clientElbPersistenceService);
-    }
-
-    @Test
-    public void testUpdateClientElb() {
-
-        Response response_1 = clientElbResource.updateClientElb("id1", null);
-        Assertions.assertThat(response_1.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
-
-        Response response_2 = clientElbResource.updateClientElb(null, "http://10.24.35.3");
-        Assertions.assertThat(response_2.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
-
-        Response response_3 = clientElbResource.updateClientElb(null, "http://10.24.35.3/api");
-        Assertions.assertThat(response_3.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
-
-        doNothing().when(clientElbPersistenceService).updateClientElb("id1", "http://10.24.32.1");
-
-        Response response_4 = clientElbResource.updateClientElb("id1", "http://10.24.32.1");
-        Assertions.assertThat(response_4.getStatus()).isEqualTo(Response.Status.ACCEPTED.getStatusCode());
-
-        verify(clientElbPersistenceService, times(1)).updateClientElb(
-                "id1", "http://10.24.32.1");
-        verifyNoMoreInteractions(clientElbPersistenceService);
-    }
-
-    @Test
-    public void testUpdateClientElb_DefaultElbIdRestriction() {
-        Response response_1 = clientElbResource.updateClientElb("defaultElbId", "http://10.24.32.1");
-        Assertions.assertThat(response_1.getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
-    }
+    verify(clientElbPersistenceService, times(1)).persistClientElb(
+        "id1", clientElbDefinition);
+    verifyNoMoreInteractions(clientElbPersistenceService);
+  }
 
 
-    @Test
-    public void testDeleteClientElb() {
+  @Test
+  public void testFindById() {
 
-        Response response_1 = clientElbResource.deleteClientElb(null);
-        Assertions.assertThat(response_1.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+    when(clientElbPersistenceService.findByIdClientElb("id2"))
+        .thenReturn("http://10.3.2.3");
 
-        doNothing().when(clientElbPersistenceService).deleteClientElb("magic_id");
-        Response response_2 = clientElbResource.deleteClientElb("magic_id");
-        Assertions.assertThat(response_2.getStatus()).isEqualTo(Response.Status.ACCEPTED.getStatusCode());
+    Response response_1 = clientElbResource.findByIdClientElb(null);
+    Assertions.assertThat(response_1.getStatus())
+        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
 
-        verify(clientElbPersistenceService, times(1)).deleteClientElb(
-                "magic_id");
-        verifyNoMoreInteractions(clientElbPersistenceService);
-    }
+    Response response_2 = clientElbResource.findByIdClientElb("id2");
+    Assertions.assertThat(response_2.getStatus()).isEqualTo(Response.Status.FOUND.getStatusCode());
 
-    @Test
-    public void testDeleteClientElb_DefaultElbIdRestriction() {
-        Response response_1 = clientElbResource.deleteClientElb("defaultElbId");
-        Assertions.assertThat(response_1.getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
-    }
+    when(clientElbPersistenceService.findByIdClientElb("id2"))
+        .thenReturn(null);
+
+    Response response_3 = clientElbResource.findByIdClientElb("id2");
+    Assertions.assertThat(response_3.getStatus())
+        .isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+
+    verify(clientElbPersistenceService, times(2)).findByIdClientElb(
+        "id2");
+    verifyNoMoreInteractions(clientElbPersistenceService);
+  }
+
+//  @Test
+//  public void testUpdateClientElb() {
+//
+//    Response response_1 = clientElbResource.updateClientElb("id1", null);
+//    Assertions.assertThat(response_1.getStatus())
+//        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+//
+//    Response response_2 = clientElbResource.updateClientElb(null, "http://10.24.35.3");
+//    Assertions.assertThat(response_2.getStatus())
+//        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+//
+//    Response response_3 = clientElbResource.updateClientElb(null, "http://10.24.35.3/api");
+//    Assertions.assertThat(response_3.getStatus())
+//        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+//
+//    doNothing().when(clientElbPersistenceService).updateClientElb("id1", "http://10.24.32.1");
+//
+//    Response response_4 = clientElbResource.updateClientElb("id1", "http://10.24.32.1");
+//    Assertions.assertThat(response_4.getStatus())
+//        .isEqualTo(Response.Status.ACCEPTED.getStatusCode());
+//
+//    verify(clientElbPersistenceService, times(1)).updateClientElb(
+//        "id1", "http://10.24.32.1");
+//    verifyNoMoreInteractions(clientElbPersistenceService);
+//  }
+//
+//  @Test
+//  public void testUpdateClientElb_DefaultElbIdRestriction() {
+//    Response response_1 = clientElbResource.updateClientElb("defaultElbId", "http://10.24.32.1");
+//    Assertions.assertThat(response_1.getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
+//  }
+//
+//
+//  @Test
+//  public void testDeleteClientElb() {
+//
+//    Response response_1 = clientElbResource.deleteClientElb(null);
+//    Assertions.assertThat(response_1.getStatus())
+//        .isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+//
+//    doNothing().when(clientElbPersistenceService).deleteClientElb("magic_id");
+//    Response response_2 = clientElbResource.deleteClientElb("magic_id");
+//    Assertions.assertThat(response_2.getStatus())
+//        .isEqualTo(Response.Status.ACCEPTED.getStatusCode());
+//
+//    verify(clientElbPersistenceService, times(1)).deleteClientElb(
+//        "magic_id");
+//    verifyNoMoreInteractions(clientElbPersistenceService);
+//  }
+//
+//  @Test
+//  public void testDeleteClientElb_DefaultElbIdRestriction() {
+//    Response response_1 = clientElbResource.deleteClientElb("defaultElbId");
+//    Assertions.assertThat(response_1.getStatus()).isEqualTo(Status.FORBIDDEN.getStatusCode());
+//  }
 }
